38c6f25885a2c5f22f16617f844947217656f9ee
AlexanderMusiienko <all.myen@gmail.com>
2016-11-27 20:12:33 +0200

    regex validate input changes

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -12,33 +12,30 @@
     $scope.boxClass = 'normal';
 
     $scope.check = function() {
-      //check on empty items
+      //filter empty items
       var arrayOfDishes = $scope.dishes.split(',').filter(function(value) {
         return value != "";
       });
 
-      arrayOfDishes.map(function(inputString) {
-        //check at only whitespaces items
-        if (inputString.match(/^\s*$/)) {
-          $scope.resultMsg = 'You have empty item at input field, please check it';
-          $scope.boxClass = 'red';
+      if (arrayOfDishes.length == 0) {
+        $scope.resultMsg = 'Please enter data first';
+        $scope.boxClass = 'red';
 
-        } else {
-
-          if (arrayOfDishes.length !== 0 && arrayOfDishes.length <= 3) {
+      } else {
+        arrayOfDishes.map(function(element) {
+          //check at only whitespaces items
+          if (element.match(/^\s*$/g)) {
+            $scope.resultMsg = 'You have empty item at input field, please check it';
+            $scope.boxClass = 'red';
+          } else if (arrayOfDishes.length !== 0 && arrayOfDishes.length <= 3) {
             $scope.resultMsg = 'Enjoy!';
             $scope.boxClass = 'green';
-
-          } else if(arrayOfDishes.length == 0) {
-            $scope.resultMsg = 'Please enter data first';
-            $scope.boxClass = 'red';
-
           } else {
             $scope.resultMsg = 'Too much!';
             $scope.boxClass = 'green';
           }
-        }
-      })
+        });
+      }
     }
   }
 
